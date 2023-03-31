f1dc906b4bd3befe5f514b58a96b330c2ae18229
Maaz Ahmed Ansari <maazansari54321@gmail.com>
2023-03-31 23:51:00 +0530

    liked lists intro

---
+++ 02-Linked Lists/1_Introduction.py
@@ -0,0 +1,12 @@
+# List have indexes
+# Linked List does not have indexes
+
+# List is a contiguous space in memory i.e. all elements are next to each other
+# Whereas in Linked List, all of nodes are spread all over the place
+
+# With linked list, have a variable called "head" which points to first node in the list
+# And a variable called "tail" which points to last item
+
+# And each node points to the next node and last node points to "None"
+# Each element/node is spread out in the memory, and since each node points to next so they can be accessed 
+# As oppossed to being contiguous space 
