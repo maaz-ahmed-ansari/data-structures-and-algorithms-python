8beedd2815a02f1604e5ae1893827c79630d181e
Maaz Ahmed Ansari <maazansari54321@gmail.com>
2023-04-02 23:55:26 +0530

    note

--- 02-Linked Lists/1_Introduction.py
+++ 02-Linked Lists/1_Introduction.py
@@ -6,7 +6,9 @@
 
 # With linked list, have a variable called "head" which points to first node in the list
 # And a variable called "tail" which points to last item
+# Note: if LL has only one node then both head and tail points to the node itself
 
 # And each node points to the next node and last node points to "None"
-# Each element/node is spread out in the memory, and since each node points to next so they can be accessed 
-# As oppossed to being contiguous space 
+# Each element/node is spread out in the memory, 
+# and since each node points to next so they can be accessed 
+# as oppossed to being contiguous space 
